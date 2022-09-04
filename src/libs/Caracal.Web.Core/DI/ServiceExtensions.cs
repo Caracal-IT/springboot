2a9ce8e9945d48f3f4c0fb27dfc59c73b935c6cb
ettienemare <ettienemare@gmail.com>
2022-09-04 11:53:45 +0200

    Refactor DI

--- src/libs/Caracal.Web.Core/DI/ServiceExtensions.cs
+++ src/libs/Caracal.Web.Core/DI/ServiceExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Caracal.Web.Core.DI;
 
 public static class ServiceExtensions {
-  public static IServiceCollection AddObjects(this IServiceCollection services, Func<Type, Type, IServiceCollection> scope, Type type) {
+  public static IServiceCollection AddObjects(this IServiceCollection services, Func<Type, Type, IServiceCollection> isolationLevel, Type type) {
     type.Assembly
         .GetTypes()
         .Where(t => t.IsAssignableTo(typeof(IInjectable)))
@@ -16,7 +16,7 @@ public static class ServiceExtensions {
        .ToList()
        .ForEach(RegisterInterface);
 
-      void RegisterInterface(Type i) => scope(i, t);
+      void RegisterInterface(Type i) => isolationLevel(i, t);
     }
 
     return services;
