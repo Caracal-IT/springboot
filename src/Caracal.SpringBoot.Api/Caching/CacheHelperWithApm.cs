13bc7f05c1caa77b3b098a53ed54b5b588fe755b
ettienemare <ettienemare@gmail.com>
2022-09-10 21:11:21 +0200

    Add redis cache

--- src/Caracal.SpringBoot.Api/Caching/CacheHelperWithApm.cs
+++ src/Caracal.SpringBoot.Api/Caching/CacheHelperWithApm.cs
@@ -1,35 +1,20 @@
 using System.Text.Json;
-using Microsoft.Extensions.Caching.Distributed;
 using StackExchange.Redis;
 
 namespace Caracal.SpringBoot.Api.Caching; 
 
 public static class CacheHelperWithApm {
-  public static async Task SetRecordAsync<T>(this IConnectionMultiplexer multiplexer,
-    string recordId,
-    T data,
-    TimeSpan? absoluteExpireTime = null,
-    TimeSpan? slidingExpireTime = null)
-  {
-    var options = new DistributedCacheEntryOptions();
-
-    options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
-    options.SlidingExpiration = slidingExpireTime;
-
+  public static async Task SetRecordAsync<T>(this IConnectionMultiplexer multiplexer, string recordId, T data, TimeSpan? absoluteExpireTime = null) {
+    var expiryTime = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
     var jsonData = JsonSerializer.Serialize(data);
     var db = multiplexer.GetDatabase();
-    await db.StringSetAsync(recordId, jsonData, options.SlidingExpiration);
+    await db.StringSetAsync(recordId, jsonData, expiryTime, When.Always);
   }
 
-  public static async Task<T?> GetRecordAsync<T>(this IConnectionMultiplexer multiplexer,
-    string recordId)
-  {
+  public static async Task<T?> GetRecordAsync<T>(this IConnectionMultiplexer multiplexer, string recordId) {
     var db = multiplexer.GetDatabase();
     var jsonData = await db.StringGetAsync(recordId);
 
-    if (!jsonData.HasValue)
-      return default(T);
-
-    return JsonSerializer.Deserialize<T>(jsonData!);
+    return !jsonData.HasValue ? default : JsonSerializer.Deserialize<T>(jsonData!);
   }
 }
